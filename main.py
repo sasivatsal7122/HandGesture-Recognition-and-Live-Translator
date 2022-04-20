fd7907d1caa19cc3536592de5355170f6996130e
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-04-20 20:29:34 +0530

    -aw

--- main.py
+++ main.py
@@ -9,12 +9,40 @@ from app_files import calc_landmark_list, draw_info_text, draw_landmarks, get_ar
 
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
-st.title("Real Time Sign Language Translator")
-st.text("Developed by Team - x")
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
 #st.set_page_config(page_title="Talku+", page_icon="🤖")
 
+st.title("Real Time Sign Language Translator")
+st.text("Developed by Team - x")
+
+
+def util(debug_image,results,keypoint_classifier,keypoint_classifier_labels):
+    if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+                pre_processed_landmark_list = pre_process_landmark(landmark_list)
+
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+                debug_image = draw_landmarks(debug_image, landmark_list)
+
+                debug_image = draw_info_text(
+                    debug_image,
+                    handedness,
+                    keypoint_classifier_labels[hand_sign_id])
+                
+                pred_letter = keypoint_classifier_labels[hand_sign_id] 
+                
+                
+    return av.VideoFrame.from_ndarray(debug_image, format="bgr24")
+
 class VideoProcessor:
+    
+    
     def recv(self, frame):
         image = frame.to_ndarray(format="bgr24")
         
@@ -49,31 +77,25 @@ class VideoProcessor:
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         
         image.flags.writeable = False
-        
+
         results = hands.process(image)
         
         image.flags.writeable = True
         
-        if results.multi_hand_landmarks is not None:
-            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
-                pre_processed_landmark_list = pre_process_landmark(landmark_list)
-
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-
-                debug_image = draw_landmarks(debug_image, landmark_list)
-
-                debug_image = draw_info_text(
-                    debug_image,
-                    handedness,
-                    keypoint_classifier_labels[hand_sign_id])
-        global pred
-        pred = keypoint_classifier_labels[hand_sign_id]
-
-        return av.VideoFrame.from_ndarray(debug_image, format="bgr24")
+        live_stream = util(debug_image,results,keypoint_classifier,keypoint_classifier_labels)
+                           
+        return live_stream
 
 col1,col2 = st.columns((2,1))
 with col1:
-    webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+    webrtc_ctx = webrtc_streamer(
+        key="opencv-filter",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=VideoProcessor,
+        async_processing=True,
+    )
 with col2:
-    st.header(f"Predicted Letter is : {pred}")
+    pass
+    #st.header(f"Predicted Letter is :", result)
+
